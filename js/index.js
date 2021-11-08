215ad4841809e473214236ab6c00c9d332c85cbf
Johan Mathew Joseph <johanmathewjoseph@gmail.com>
2021-11-08 23:12:15 +0530

    💄 Update CSS

--- js/index.js
+++ js/index.js
@@ -11,4 +11,21 @@ $(document).ready(function ()
         $(".navbar").removeClass("navbar-sticky");
       }
     });
-});  
+});
+console.log(".");
+console.log(".");
+console.log("Hello there, fellow inspector");
+console.log(".");
+console.log(".");
+console.log("Welcome to Johan's Portfolio Website")
+console.log("Designed and Developed by Johan Mathew Joseph")
+console.log(".");
+console.log(".");
+function userEntry()
+{
+    event.preventDefault();
+    document.getElementById("contact-main-information-form-name").value = null;
+    document.getElementById("contact-main-information-form-email").value = null;
+    document.getElementById("contact-main-information-form-message").value = null;
+    alert("Submission successful")
+}
